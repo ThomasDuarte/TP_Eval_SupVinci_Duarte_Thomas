7f1d3e7de6be9f0b9e9d0734de4f6a60bdd4ddec
unknown <duarte.thomas1301@gmail.com>
2022-01-28 13:58:14 +0100

    Mise en place du bouton favoris et jeux clickables

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,7 @@
-
 window.onload = initialisation;
 let sectionPrincipale = document.querySelector("section");
 let mapJeux = new Map();
+let mesJeux = new Map();
 
 function initialisation() {
     //Récupérer l'élément HTML sectionPrincipale
@@ -17,15 +17,19 @@ function initialisation() {
     //Et y ajouter un évènement avec onclick ou addEventListener
     boutonValider.addEventListener("click", afficherListeJeux);
 
+    //Récupérer mon bouton favoris
+    const boutonFavoris = document.querySelector(".boutonFavoris");
+    if (!boutonFavoris) {
+        throw new Error("boutonFavoris introuvable");
     }
-
-
+    //Et y ajouter un évènement avec onclick ou addEventListener
+    boutonFavoris.addEventListener("click", afficherListeFavoris);
+    }
 
 async function telechargerDonnees() {
     mapJeux.clear();
     //On récupère l'input barreRecherche
     const saisieBarreRecherche = document.querySelector(".barreRecherche").value;
-    //console.log(saisieBarreRecherche);
     //On fait le fetch en utilisant l'url de l'api et en y insérant la recherche
 
     let urlApi = "https://www.giantbomb.com/api/games/?api_key=1b6fb650c9161a0081d213a2a2efa859fb367212&format=json&filter=name:";
@@ -68,6 +72,7 @@ async function telechargerDonnees() {
             }
             //J'apelle afficherListeJeux() en lui donnant mon tableau en paramètres
 */
+            console.log(mapJeux);
             return mapJeux;
         }else{
             alert("Veuillez saisir le nom de jeux à rechercher");
@@ -79,17 +84,19 @@ async function telechargerDonnees() {
     }
 }
 
-async function afficherListeJeux(tableauJeux) {
+async function afficherListeJeux() {
     let sectionPrincipale = document.querySelector("section");
     //Je vide sectionPrincipale sectionPrincipale.innerHTML = "";
     sectionPrincipale.innerHTML = "";
     //Mettre Loader (Penser a le Styliser)
     const loader = document.createElement("div");
     loader.classList.add("loader");
+    sectionPrincipale.innerHTML = "";
     sectionPrincipale.append(loader);
 
     const mapJeux = await telechargerDonnees();
-
+    console.log(mapJeux);
+    sectionPrincipale.innerHTML = "";
     const conteneur = document.createElement("div");
     conteneur.classList.add("conteneur");
     sectionPrincipale.append(conteneur);
@@ -102,18 +109,12 @@ async function afficherListeJeux(tableauJeux) {
             <div class="plateformes"></div>
             <h3>${jeu.nom}</h3>
         `;
-
-        conteneur.append(apercuJeu);
-
         //Récupérer mon bouton apercuJeu
-        const boutonApercuJeu = conteneur.querySelector(".apercuJeu");
-        if (!boutonApercuJeu) {
-            throw new Error("boutonApercuJeu introuvable");
-        }
         //Et y ajouter un évènement avec onclick ou addEventListener
-        boutonApercuJeu.addEventListener("click", () =>{
+        apercuJeu.addEventListener("click", () =>{
             afficherJeu(jeu);
         });
+        conteneur.append(apercuJeu);
     });
 
     /*
@@ -146,9 +147,10 @@ function afficherJeu(jeu) {
     //Je détermine son innerHTML
     divFicheJeu.innerHTML = `
         <div class="premierePartieFiche">
-                        <h2>Nom du jeu</h2>
-                        <button>Ajouter aux favoris</button>
-                        <img src="https://www.giantbomb.com/a/uploads/scale_small/11/110673/3026329-gb_default-16_9.jpg">
+                        <h2>${jeu.nom}</h2>
+                        <button class="ajouterFavoris">Ajouter aux favoris</button>
+                        <button class="retirerFavoris">Retirer des favoris</button>
+                        <img src=${jeu.imageMiniature}>
                     </div>
                     <div class="deuxiemePartieFiche">
                         <div class="plateformes">
@@ -158,17 +160,102 @@ function afficherJeu(jeu) {
                             <p>PC</p>
                             <p>+99</p>
                         </div>
-                        <h3>Sortie : 1997</h3>
+                        <h3>${jeu.dateDeSortie}</h3>
                     </div>
                     <div class="descriptionCourte">
-                        <p>A top-down isometric helicopter shoot 'em up originally for the Sega Genesis, which was later ported to a variety of platforms. It is best known for its open-ended mission design and was followed by several sequels.</p>
+                        ${jeu.descriptionCourte}
                     </div>
                     <div class="descriptionComplete">
-                        <h2>Overview</h2><p>Desert Strike: Return to the Gulf is an isometric helicopter shoot 'em up from Electronic Arts. The player is a fighter pilot who must take down mission-critical targets across a number of maps. The player is free to pursue these missions in any order, and must also keep an eye on the fuel, damage, and ammo gauges.</p><p>Desert Strike is the first of the prolific <a href=\"/strike-series/3025-143/\" data-ref-id=\"3025-143\">Strike</a> series, and was followed with <a href=\"/jungle-strike/3030-2180/\" data-ref-id=\"3030-2180\">Jungle Strike</a> and <a href=\"/urban-strike/3030-9336/\" data-ref-id=\"3030-9336\">Urban Strike</a> on the Genesis. Two more games, <a href=\"/soviet-strike/3030-2422/\" data-ref-id=\"3030-2422\">Soviet Strike</a> and <a href=\"/nuclear-strike/3030-11731/\" data-ref-id=\"3030-11731\">Nuclear Strike</a>, were released later for 32-bit systems.</p><h2>Story</h2><p>A year after the Gulf War, General Ibn Kilbaba takes over a small <a href=\"/unnamed-middle-eastern-location/3035-179/\" data-ref-id=\"3035-179\">Arab emirate</a> and plans to start World War III. Using an <a href=\"/ah-64-apache/3055-2463/\" data-ref-id=\"3055-2463\">AH-64 Apache</a>, the player must open the way for ground troops and finally take on the \"Madman\" himself.</p><h2>Gameplay</h2><p>The game is played from an isometric perspective in open levels that allow free movement in all directions by scrolling the screen with the movement of the helicopter. Each level consists of many varying objectives that range anywhere from destroying enemy bases and vehicles to capturing enemy troops or rescuing friendly ones. While bases and vehicles are simply destroyed, both friendly and enemy troops must be taken back to base for extraction. The AH-64 Apache has limited cargo space, so multiple trips to and from the base may be necessary. These objectives aren't always linear, and can often be tackled in whatever order the player chooses. This combination of free movement and non-linear structure separated Desert Strike from many of the other contemporary shooters.</p><p>There are three weapons of varying strength and usefulness available to the AH-64 Apache: machine guns, hydra missiles, and hellfire missiles, which increase in strength respectively. Each of these weapons has a limited number of ammo which can only be replenished by picking up ammo crates on the mission or by resupplying back at the base. Similarly, the AH-64 Apache only has a limited amount of fuel that will drain slowly over the course of each level. If the fuel runs out, the helicopter crashes and the player loses a life. Refueling works exactly the same as restocking ammo.</p><p>Lives are lost when either the AH-64 Apache takes too much damage and is destroyed, or when it runs out of fuel. After three lives have been lost, the game is over. Due to the nature of the game's freedom, each level requires a certain amount of planning and strategy in order to complete all of the objectives while still having enough fuel, ammo, and health to survive.</p><h2>Ports</h2><p>Due to its popularity on the <a href=\"/sega-genesis/3055-4659/\" data-ref-id=\"3055-4659\">Sega Genesis</a> in 1992, the game was then ported to the <a href=\"/amiga/3045-1/\" data-ref-id=\"3045-1\">Amiga</a>, <a href=\"/sega-master-system/3045-8/\" data-ref-id=\"3045-8\">Master System</a>, and <a href=\"/snes/3045-9/\" data-ref-id=\"3045-9\">SNES</a> in the same year. Two years later in 1994, it was released on the <a href=\"/pc/3045-94/\" data-ref-id=\"3045-94\">PC</a>. It was also ported to most handheld systems such as the <a href=\"/lynx/3045-7/\" data-ref-id=\"3045-7\">Lynx</a> in 1993, <a href=\"/game-gear/3045-5/\" data-ref-id=\"3045-5\">Game Gear</a> in 1994, <a href=\"/game-boy/3045-3/\" data-ref-id=\"3045-3\">Game Boy</a> in 1995, <a href=\"/game-boy-advance/3045-4/\" data-ref-id=\"3045-4\">Game Boy Advance</a> in 2002, and finally the <a href=\"/psp/3045-18/\" data-ref-id=\"3045-18\">PSP</a> in 2006 as part of <a href=\"/ea-replay/3030-10965/\" data-ref-id=\"3030-10965\">EA Replay</a>.</p>
+                        ${jeu.descriptionComplete}
                     </div>        
     `;
+
+    const boutonAjouter = divFicheJeu.querySelector(".ajouterFavoris");
+    const boutonRetirer = divFicheJeu.querySelector(".retirerFavoris");
+
+    boutonAjouter.addEventListener("click", () =>{
+        ajouterAMesFavoris(jeu);
+    });
+
+    boutonRetirer.addEventListener("click", () =>{
+        retirerJeu(jeu);
+    });
+
     sectionPrincipale.append(divFicheJeu);
     //J'append dans sectionPrincipale
+}
+
+function ajouterAMesFavoris(jeu) {
+    mesJeux.set(jeu.guId, jeu);
+    sauvegarderMesJeux();
+}
+
+function sauvegarderMesJeux() {
+    const tableauMesJeux = Array.from(mesJeux.values());
+    window.localStorage.setItem("mesJeux", JSON.stringify(tableauMesJeux));
+}
+
+function chargerMesJeux() {
+    mesJeux = new Map();
+    const json = window.localStorage.getItem("mesJeux");
+    if (!json) {
+        return mesJeux;
+    }
+    const tableauParse = JSON.parse(json);
+    tableauParse.forEach(jeuObj =>{
+        const jeuFavoris = new Jeu(jeuObj.fichierJson);
+        mesJeux.set(jeuFavoris.guId, jeuFavoris);
+    });
+    return mesJeux;
+}
+
+function retirerJeu(monJeu) {
+    mesJeux.delete(monJeu.guId);
+    sauvegarderMesJeux();
+}
+
+function afficherListeFavoris(){
+
+    let sectionPrincipale = document.querySelector("section");
+    //Je vide sectionPrincipale sectionPrincipale.innerHTML = "";
+    sectionPrincipale.innerHTML = "";
+    //Mettre Loader (Penser a le Styliser)
+    const loader = document.createElement("div");
+    loader.classList.add("loader");
+    sectionPrincipale.innerHTML = "";
+    sectionPrincipale.append(loader);
+
+    const mapJeux = chargerMesJeux();
+    console.log(mapJeux);
+
+    if(mapJeux.size > 0) {
+        sectionPrincipale.innerHTML = "";
+        const conteneur = document.createElement("div");
+        conteneur.classList.add("conteneur");
+        sectionPrincipale.append(conteneur);
+
+        mapJeux.forEach(jeu => {
+            const apercuJeu = document.createElement("div");
+            apercuJeu.classList.add("apercuJeu");
+            apercuJeu.innerHTML = `
+            <img src="${jeu.image}">
+            <div class="plateformes"></div>
+            <h3>${jeu.nom}</h3>
+        `;
+            //Récupérer mon bouton apercuJeu
+            //Et y ajouter un évènement avec onclick ou addEventListener
+            apercuJeu.addEventListener("click", () => {
+                afficherJeu(jeu);
+            });
+            conteneur.append(apercuJeu);
+        });
+    }else{
+        sectionPrincipale.innerHTML=`
+            <div style=" width: 100%; text-align: center">
+            <h2 style="color: black"">Aucun jeu en favoris</h2>
+            </div>
+        `;
+    }
 }
 
 class Jeu{
@@ -187,6 +274,7 @@ class Jeu{
 
     constructor(fichierJson) {
 
+        this.fichierJson = fichierJson;
         this.nom = fichierJson.name;
         this.plateformes = fichierJson.platforms;
         //this.nbPlateformes = this.plateformes.length;
@@ -195,9 +283,9 @@ class Jeu{
         this.guId = fichierJson.guid;
 
         if (fichierJson.original_release_date !== null) {
-            this.dateDeSortie = fichierJson.original_release_date;
+            this.dateDeSortie = "Sortie : " + fichierJson.original_release_date;
         } else {
-            this.dateDeSortie = fichierJson.expected_release_year;
+            this.dateDeSortie = "Sortie prévue : " + fichierJson.expected_release_year;
         }
 
         this.descriptionCourte = fichierJson.deck;
